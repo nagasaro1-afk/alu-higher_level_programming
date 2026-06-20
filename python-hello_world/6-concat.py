9451b3d536b6cf39d5090977b911536b171e994c
nagasaro1-afk <n.agasaro1@alustudent.com>
2026-06-20 09:30:48 -0700

    Add python-hello_world project files

---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str1 = "Welcome to "
+str2 = "School!"
+print(str1 + "Holberton " + str2)
---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+word = "object-oriented programming with Python"
+language = "programming"
+print(word[0:16] + language + word[27:])
